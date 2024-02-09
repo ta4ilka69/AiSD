f5b1503736a80e937fc4f5d0466a7143994042af
ta4ilka69 <artem_balin_2004@mail.ru>
2024-02-10 01:14:23 +0300

    2 start

---
+++ contest/2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+
+int main()
+{
+	stack<char> current;
+	string animals;
+	cin >> animals;
+	for (size_t i = 0; i < animals.length(); i++) {
+		if (current.size() == 0) {
+			current.push(animals[i]);
+		}
+		else {
+			char last = current.top();
+			if (isupper(last)) {
+				if (isupper(animals[i])) {
+					current.push(animals[i]);
+				}
+			}
+		}
+	}
+	return 0;
+}
