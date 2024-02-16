52236cda01213f52d686a5774f0a48c9f41e5f46
ta4ilka69 <artem_balin_2004@mail.ru>
2024-02-16 03:44:10 +0300

    5 start

---
+++ 5/5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N, K;
+	cin >> N; cin >> K;
+	vector<int> x(N), dx, dx_sorted;
+	for (size_t i = 0; i < N; ++i) {
+		cin >> x[i];
+		if (i != 0) {
+			dx.push_back(x[i] - x[i - 1]);
+			dx_sorted.push_back(x[i] - x[i - 1]);
+		}
+	}
+	sort(dx_sorted.begin(), dx_sorted.end());
+	for (int i = 0; i < K; i++) {
+		for (int j = 0; j < N - 1; j++) {
+			if (dx_sorted[i] == dx[j]) {
+				if (j != N - 1) {
+					dx[j + 1] += dx[j];
+					dx.erase(dx.begin() + j);
+				}
+				else {
+					dx.pop_back();
+				}
+				break;
+			}
+		}
+	}
+
+}
