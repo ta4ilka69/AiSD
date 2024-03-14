aa36d0b6ef3e0993ce806569d76795e374c0d725
ta4ilka69 <artem_balin_2004@mail.ru>
2024-03-14 21:20:06 +0300

    5-

--- 5/5.cpp
+++ 5/5.cpp
@@ -1,12 +1,15 @@
 ﻿#include <iostream>
 #include <vector>
+#include <algorithm>
+
+#define int_max 2147483647
 using namespace std;
 
 int main()
 {
-	int N, K,min;
+	int N, K, min;
 	cin >> N; cin >> K;
-	min = 2147483647;
+	min = int_max;
 	vector<int> x(N), dx;
 	for (size_t i = 0; i < N; ++i) {
 		cin >> x[i];
@@ -17,19 +20,41 @@ int main()
 			}
 		}
 	}
-	for (int i = 0; i < N-K; i++) {
-		for (int j = 0; j < dx.size(); j++) {
-			if (min == dx[j]) {
-				dx[j + 1] += dx[j];
-				dx.erase(dx.begin()+j);
-				min = *min_element(dx.begin(),dx.end());
-				break;
+	for (size_t i = 0; i < N - K; i++) {
+		for (size_t j = 0; j < dx.size(); j++) {
+			if (j != dx.size() - 1 && min == dx[j]) {
+				if (j != 0) {
+					if (dx[j + 1] > dx[j - 1]) {
+						dx[j - 1] += dx[j];
+					}
+					else {
+						dx[j + 1] += dx[j];
+					}
+					dx.erase(dx.begin() + j);
+					min = *min_element(dx.begin(), dx.end());
+					break;
+				}
+				else {
+					dx[j + 1] += dx[j];
+					dx.erase(dx.begin() + j);
+					min = *min_element(dx.begin(), dx.end());
+					break;
+
+				}
+			}
+			else {
+				if (min == dx[j]) {
+					dx[j - 1] += dx[j];
+					dx.erase(dx.begin() + j);
+					min = *min_element(dx.begin(), dx.end());
+					break;
+				}
 			}
 		}
 	}
 	cout << min;
 }
 /*
-3 5
-1 2 3 5 9
+9 5
+1 4 6 8 9 13 30 100 190
 */
