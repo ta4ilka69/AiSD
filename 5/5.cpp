1476a9cf5737ab58028ac2869744eb85c63b89b5
ta4ilka69 <artem_balin_2004@mail.ru>
2024-03-26 00:37:30 +0300

    finally 5 блять

--- 5/5.cpp
+++ 5/5.cpp
@@ -2,59 +2,47 @@
 #include <vector>
 #include <algorithm>
 
-#define int_max 2147483647
 using namespace std;
 
 int main()
 {
-	int N, K, min;
+	int N, K;
 	cin >> N; cin >> K;
-	min = int_max;
-	vector<int> x(N), dx;
+	vector<int> x(N);
 	for (size_t i = 0; i < N; ++i) {
 		cin >> x[i];
-		if (i != 0) {
-			dx.push_back(x[i] - x[i - 1]);
-			if (dx[i - 1] < min) {
-				min = dx[i - 1];
-			}
-		}
 	}
-	for (size_t i = 0; i < N - K; i++) {
-		for (size_t j = 0; j < dx.size(); j++) {
-			if (j != dx.size() - 1 && min == dx[j]) {
-				if (j != 0) {
-					if (dx[j + 1] > dx[j - 1]) {
-						dx[j - 1] += dx[j];
-					}
-					else {
-						dx[j + 1] += dx[j];
-					}
-					dx.erase(dx.begin() + j);
-					min = *min_element(dx.begin(), dx.end());
-					break;
-				}
-				else {
-					dx[j + 1] += dx[j];
-					dx.erase(dx.begin() + j);
-					min = *min_element(dx.begin(), dx.end());
-					break;
-
-				}
-			}
-			else {
-				if (min == dx[j]) {
-					dx[j - 1] += dx[j];
-					dx.erase(dx.begin() + j);
-					min = *min_element(dx.begin(), dx.end());
+	int left = 1;
+	int right = x[N - 1];
+	int mid = 0;
+	bool T = false;
+	while (left <= right) {
+		if (left - right == -1) {
+			T = true;
+		}
+		mid = (left + right) / 2;
+		int last = x[0];
+		int k = K-1;
+		for (int i = 1; i < N; i++) {
+			if (x[i] - last >= mid) {
+				last = x[i];
+				k--;
+				if (k == 0) {
+					left = mid;
 					break;
 				}
 			}
 		}
+		if (k != 0) {
+			right = mid;
+		}
+		if (T) {
+			break;
+		}
 	}
-	cout << min;
+	cout << mid;
 }
 /*
-9 5
-1 4 6 8 9 13 30 100 190
+10 6
+1 2 3 100 1000 1900 1997 1998 1999 2000
 */
