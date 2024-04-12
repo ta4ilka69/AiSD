6c8a687c108571d2976fbbce2d9f41497c2f065b
ta4ilka69 <artem_balin_2004@mail.ru>
2024-04-12 14:06:47 +0300

    Update 9.cpp

--- 9/9.cpp
+++ 9/9.cpp
@@ -1,12 +1,56 @@
 ﻿#include <iostream>
-#include <unordered_set>
+#include <queue>
+#include <vector>
 #include <list>
 using namespace std;
+struct honda {
+	int value;
+	int last;
+	bool ispresent;
+	bool operator < (const honda& a) const
+	{
+		if (ispresent) {
+			return last < a.last;
+		}
+		return false;
+	}
+};
+
 int main()
 {
 	int n, k, p;
 	cin >> n >> k >> p;
-	int s = 0;
-	//хранить в векторе очереди с индексами, где находятся числа, удалять из текущего set'а тот, который имеет более дальний следующий индекс
+	int s = k;
+	vector <list<int>> pos(n);
+	vector<int> values(n);
+	for (int i = 0; i < p; i++)
+	{
+		int x;
+		cin >> x;
+		values[i] = x;
+		pos[values[i] - 1].push_back(i);
+	}
+	priority_queue<honda> prev;
+	int i = 0;
+	while (prev.size() < k && i < p) {
+		int x = values[i];
+		if (pos[x - 1].size() > 1) {
+			pos[x - 1].pop_front();
+			prev.emplace({ x, pos[x - 1].front(), true });
+		}
+		else {
+			pos[x - 1].pop_front();
+			prev.emplace({ x, -1, false });
+		}
+		i++;
+	}
+	while (i < p) {
+		int x = values[i];
+		if (pos[x - 1].size() > 0) {
+			if (prev.find({ x,pos[x - 1].front(),true }) == prev.end()) {
+
+			}
+		}
+	}
 	cout << s << endl;
 }
