32e20c19003383cb24d4b09a37d3ce1ad0a0164d
ta4ilka69 <artem_balin_2004@mail.ru>
2024-04-12 04:26:55 +0300

    9-...

---
+++ 9/9.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <list>
+using namespace std;
+int main()
+{
+	int n, k, p;
+	cin >> n >> k >> p;
+	int s = 0;
+	//хранить в векторе очереди с индексами, где находятся числа, удалять из текущего set'а тот, который имеет более дальний следующий индекс
+	cout << s << endl;
+}
